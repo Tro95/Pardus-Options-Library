eb9864d2bb4a7e3c41c390cfd1ff7dfb1562d228
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2020-02-03 13:53:06 +0000

    Initial push from gist

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Pardus-Options-Library
 The Pardus Options Library is a script designed to add useful and easy ways to interact with  the Options page within other scripts related to the online game Pardus (https://pardus.at).
+
+Instructions:
+ - At the top of your Tampermonkey script, ensure you have the following lines:
+```
+// @grant           unsafeWindow
+// @require         https://gist.github.com/Tro95/3b102f4b834682bd2d2793b66e47845a/raw/pardus_options.js
+```
+This will allow the unsafeWindow permision within your script, which is necessary to guarantee safe
+interactions with other scripts utilising this library also. This will also include the library
+itself, embedding it within the unsafeWindow object.
+
+ - Ensure your script is allowed to run on the Options page:
+```        
+// @include http*://*.pardus.at/options.php
+```
+ - Within your script, you should ensure you check `document.location.pathname` to see if you're on the `options.php` page, as the library is only defined and available on this page.
+
+```
+if (document.location.pathname == '/options.php') {
+    // Option-related logic goes here
+}
+```
+ - The library is available through a singleton object `unsafeWindow.Options`.
+
+ - Add a new tab to the Options page. Recommended usage is one tab per script or userspace.
+
+```
+unsafeWindow.Options.addNewTab(label, id)
+```                    
+label:  String to display in the tab. Visual limitations means this can only be a maximum
+        of two words, 10 characters each.
+id:     HTML element id for the tab. This should be a string with no whitespace, convention
+        is to all be lowercase and words separated with hyphens. 
+
+Returns:    OptionsContext object. This should be assigned to a variable that is then used
+            to add options boxes into the tab.
+
+ - Get an existing tab on the Options page. This is for scripts that wish to share the same tab.
+
+```
+unsafeWindow.Options.getTab(id)
+```
+id:     HTML element id for the tab you wish to return.
+
+Returns:    OptionsContext object of the existing tab. This should be assigned to a variable
+            that is then used to add options boxes into the tab.
---
+++ pardus_options_library.js
@@ -0,0 +1,192 @@
+var Options = (function() {
+    'use strict';
+
+    var tabs = [];
+    var tabs_element = null;
+    var content_element = null;
+
+    function initialise() {
+
+        // HTML for both the options tabs and the content area
+        var options_html = `<table id="options-area"><tbody><tr><td><table id="options-tabs" width="96" cellspacing="0" cellpadding="0" border="0" align="left"><tbody><tr></tr></tbody></table></td></tr><tr id="options-content"><td></td></tr></tbody></table>`;
+
+        // Element for both the options tabs and the content area
+        var optionsElement = htmlToElement(options_html);
+
+        var default_pardus_options_content = document.getElementsByTagName('table')[2];
+        var pardus_main_element = default_pardus_options_content.parentNode;
+
+        default_pardus_options_content.setAttribute("id", "options-content-pardus-default");
+        default_pardus_options_content.setAttribute("hidden", "");
+
+        optionsElement.childNodes[0].childNodes[1].childNodes[0].appendChild(default_pardus_options_content);
+
+        pardus_main_element.appendChild(optionsElement);
+        tabs_element = document.getElementById('options-tabs');
+        content_element = document.getElementById('options-content');
+
+        addTab('Pardus Options', 'pardus-default', default_pardus_options_content);
+    }
+
+    function addTab(label, id, content) {
+        var tmp_content_element = null;
+
+        if (content) {
+            tmp_content_element = content;
+        } else {
+            tmp_content_element = createTabContentElement(label, id);
+        }
+
+        var new_tab = {
+            active: false,
+            label: label,
+            id: id.toString(),
+            content_element: tmp_content_element,
+            tab_element: createTabElement(label, id)
+        };
+
+        new_tab.tab_element.addEventListener("click", function(){setAsActive(new_tab.id);}, true);
+        tabs_element.childNodes[0].childNodes[0].appendChild(new_tab.tab_element);
+        tabs_element.width = (tabs.length + 1) * 96;
+        content_element.childNodes[0].appendChild(new_tab.content_element);
+        tabs.push(new_tab);
+
+        return new OptionsContent(new_tab.id);
+    }
+
+    function setAsActive(id) {
+        for (var i = 0; i < tabs.length; i++) {
+            if (tabs[i].id != id) {
+                tabs[i].tab_element.setAttribute("style", "background: transparent url('//static.pardus.at/img/std/tab.png') repeat scroll 0% 0%; cursor: default;");
+                tabs[i].tab_element.setAttribute("onmouseover", "this.style.background='url(//static.pardus.at/img/std/tabactive.png)';this.style.cursor='default'");
+                tabs[i].tab_element.setAttribute("onmouseout", "this.style.background='url(//static.pardus.at/img/std/tab.png)'");
+                tabs[i].content_element.setAttribute("hidden", "");
+                tabs[i].active = false;
+            } else {
+                tabs[i].tab_element.setAttribute("style", "background: transparent url('//static.pardus.at/img/std/tabactive.png') repeat scroll 0% 0%; cursor: default;");
+                tabs[i].tab_element.setAttribute("onmouseover", "this.style.cursor='default'");
+                tabs[i].tab_element.removeAttribute("onmouseout");
+                tabs[i].content_element.removeAttribute("hidden");
+                tabs[i].active = true;
+            }
+        }
+    }
+
+    function createTabElement(label, id) {
+        return htmlToElement(`<td id="` + id.toString() + `" style="background: transparent url(&quot;//static.pardus.at/img/std/tab.png&quot;) repeat scroll 0% 0%; cursor: default;" onmouseover="this.style.background='url(//static.pardus.at/img/std/tabactive.png)';this.style.cursor='default'" onmouseout="this.style.background='url(//static.pardus.at/img/std/tab.png)'" class="tabcontent">` + label + `</td>`);
+    }
+
+    function createTabContentElement(label, id) {
+        return htmlToElement(`<table hidden class="messagestyle" id="options-content-` + id.toString() + `" style="background:url(//static.pardus.at/img/std/bgdark.gif)"><tbody><tr><td><div align="center"><h1>` + label + `</h1></div><div id="saved-message" align="center" hidden=""><h2><font style="border:2px; border-style:solid; border-radius:10px; border-color:#00AA00; padding: 5px;" color="green">Settings saved</font></h2></div><table width="100%" align="center"><tbody><tr><td id="options-content-` + id.toString() + `-left" width="350" valign="top"></td><td width="40"></td><td id="options-content-` + id.toString() + `-right" width="350" valign="top"></td></tr></tbody></table></td></tr></tbody></table>`);
+    }
+
+    function drawTabs() {
+        tabs_element.width = tabs.length * 96;
+        tabs_element.childNodes[0].childNodes[0].innerHTML = "";
+        for (var i = 0; i < tabs.length; i++) {
+            tabs_element.childNodes[0].childNodes[0].appendChild(tabs[i].tab_element);
+        }
+    }
+
+    //initialise();
+    //setAsActive('pardus-default');
+
+    return {
+        addNewTab: function(label, id) {
+            return addTab(label, id, null);
+        },
+        getTab: function(id) {
+            for (var i = 0; i < tabs.length; i++) {
+                if (tabs[i].id == id) {
+                    return tabs[i];
+                }
+            }
+        },
+        create: function() {
+            initialise();
+            setAsActive('pardus-default');
+        }
+    };
+
+})();
+
+function OptionsContent(id) {
+    this.id = id;
+    this.left_boxes = [];
+    this.right_boxes = [];
+    this.left_element = document.getElementById('options-content-' + id.toString() + '-left');
+    this.right_element = document.getElementById('options-content-' + id.toString() + '-right');
+    this.addBox = function(heading, premium = false) {
+        var new_box = null;
+        if (this.left_boxes.length <= this.right_boxes.length) {
+            return this.addBoxLeft(heading, premium);
+        } else {
+            return this.addBoxRight(heading, premium);
+        }
+    };
+    this.addBoxLeft = function(heading, premium = false) {
+        var new_box = new OptionsBox(id + '-left', this.left_boxes.length, heading, premium);
+        this.left_element.appendChild(new_box.element);
+        this.left_element.appendChild(document.createElement('br'));
+        this.left_element.appendChild(document.createElement('br'));
+        this.left_boxes.push(new_box);
+        return new_box;
+    };
+    this.addBoxRight = function(heading, premium = false) {
+        var new_box = new OptionsBox(id + '-right', this.right_boxes.length, heading, premium);
+        this.right_element.appendChild(new_box.element);
+        this.right_element.appendChild(document.createElement('br'));
+        this.right_element.appendChild(document.createElement('br'));
+        this.right_boxes.push(new_box);
+        return new_box;
+    };
+    this.addPremiumBox = function(heading) {
+        return this.addBox(heading, true);
+    };
+    this.addPremiumBoxLeft = function(heading) {
+        return this.addBoxLeft(heading, true);
+    };
+    this.addPremiumBoxRight = function(heading) {
+        return this.addBoxRight(heading, true);
+    };
+}
+
+function OptionsBox(id, number, heading, premium = false) {
+    var header_html = '<th>';
+    if (premium == true) {
+        header_html = '<th class="premium">';
+    }
+    this.id = 'options-content-' + id.toString() + '-box-' + number.toString();
+    this.front_container = `<form id="` + this.id + `" action="none"><table style="background:url(//static.pardus.at/img/std/bgd.gif)" width="100%" cellpadding="3" align="center"><tbody><tr>` + header_html + heading + `</th></tr>`;
+    this.back_container = `</tbody></table></form>`;
+    this.inner_html = ``;
+    this.element = htmlToElement(this.front_container + this.inner_html + this.back_container);
+    this.refreshElement = function() {
+        this.element = htmlToElement(this.front_container + this.inner_html + this.back_container);
+        document.getElementById(this.id).replaceWith(this.element);
+    };
+    this.setInnerHTML = function(inner_html_to_set) {
+        this.inner_html = inner_html_to_set;
+        this.refreshElement();
+    };
+}
+
+/**
+ *  Helper functions
+ */
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+ /**
+  *  Add the Options object to the page for all scripts to use
+  */
+if (document.location.pathname == '/options.php') {
+    if(typeof unsafeWindow.Options === 'undefined'|| !unsafeWindow.Options){
+        (function(){
+            unsafeWindow.Options = Options;
+            unsafeWindow.Options.create();
+        })();
+    }
+}
